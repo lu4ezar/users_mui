477954dece88c4d066d4019bb0d1076bde712b30
lu4ezar <misha-shisha@mail.ru>
2020-04-22 21:31:21 +0700

    users: updated imports

--- components/users.jsx
+++ components/users.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
 import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Fab from "@material-ui/core/Fab";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Fab,
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles({
