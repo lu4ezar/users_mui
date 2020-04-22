25188bdb3fdccceb15772a0f7babc599b4ef328f
lu4ezar <misha-shisha@mail.ru>
2020-04-22 23:42:07 +0700

    update _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,19 @@
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
-/* eslint react/prop-types: 0 */
+import PropTypes from "prop-types";
 import "../styles/global.css";
+import { CssBaseline } from "@material-ui/core";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
