f74b33f6cf62a64b0bc5a94436f9dfc101aec22f
lu4ezar <misha-shisha@mail.ru>
2020-04-22 21:27:58 +0700

    updated users

--- components/users.jsx
+++ components/users.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -9,47 +10,74 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
-import useFetchUsers from "../hooks/useFetchUsers";
 
 const useStyles = makeStyles({
+  root: {
+    position: "relative",
+  },
   table: {
-    minWidth: 0,
+    minWidth: 600,
+  },
+  fab: {
+    position: "absolute",
+    top: 0,
+    right: 0,
   },
 });
 
-const Users = () => {
-  const users = useFetchUsers();
+const Users = ({ users }) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <h1>Users</h1>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} size="small" aria-label="users table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="right">Email</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.id}>
-                <TableCell component="th" scope="row">
-                  {user.name}
-                </TableCell>
-                <TableCell align="right">{user.email}</TableCell>
+      {!users ? null : (
+        <TableContainer component={Paper}>
+          <Table
+            stickyHeader
+            className={classes.table}
+            size="small"
+            aria-label="users table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Name</TableCell>
+                <TableCell align="right">Email</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {users.map((user) => (
+                <TableRow hover key={user.id}>
+                  <TableCell component="th" scope="row">
+                    {user.name}
+                  </TableCell>
+                  <TableCell align="right">{user.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
       <Link href="user/add">
-        <Fab aria-label="add user">
+        <Fab className={classes.fab} aria-label="add user">
           <AddIcon />
         </Fab>
       </Link>
     </div>
   );
+};
+
+Users.propTypes = {
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      email: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
+};
+
+Users.defaultProps = {
+  users: [],
 };
 
 export default Users;
