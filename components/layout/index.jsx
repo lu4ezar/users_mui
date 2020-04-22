4ad2f36c30c488d10ece22d4729b911716d5cc55
lu4ezar <misha-shisha@mail.ru>
2020-04-22 21:27:26 +0700

    changed description

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -12,10 +12,7 @@ export default function Layout({ children, home }) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+        <meta name="description" content="NextJS GraphQL CRUD app" />
         <meta
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
