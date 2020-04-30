2069bfa5374c6d57c3bb81ea915f818461981374
lu4ezar <misha-shisha@mail.ru>
2020-04-30 15:24:22 +0700

    config json => js

---
+++ server/db/config.js
@@ -0,0 +1,12 @@
+export default {
+  development: {
+    development: {
+      url: "localhost",
+      port: "27017",
+      database: "users",
+      user: "",
+      password: "",
+    },
+  },
+  production: {},
+};
