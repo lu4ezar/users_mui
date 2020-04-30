28a0d8c34388a18f88671987a3a714b4ebebe49b
lu4ezar <misha-shisha@mail.ru>
2020-04-30 15:59:52 +0700

    fix typo in config

--- server/db/config.js
+++ server/db/config.js
@@ -1,12 +1,10 @@
 export default {
   development: {
-    development: {
-      url: "localhost",
-      port: "27017",
-      database: "users",
-      user: "",
-      password: "",
-    },
+    url: "localhost",
+    port: "27017",
+    database: "users",
+    user: "",
+    password: "",
   },
   production: {},
 };
