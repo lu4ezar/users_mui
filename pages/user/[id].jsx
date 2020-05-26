83f849e8bc4b646c546d6fac63690d12a7903162
lu4ezar <misha-shisha@mail.ru>
2020-05-26 10:16:55 +0700

    refactor user [id] dynamic route

--- pages/user/[id].jsx
+++ pages/user/[id].jsx
@@ -1,36 +1,42 @@
-import { useRouter } from "next/router";
-import { useQuery } from "@apollo/react-hooks";
+import PropTypes from "prop-types";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import Layout from "../../src/components/layout";
-import { GET_USER } from "../../src/apolloClient";
+import client, { GET_USER } from "../../src/apolloClient";
 
-/*
-initial render results in failed graphql query:
-https://nextjs.org/docs/routing/dynamic-routes#caveats
-https://github.com/zeit/next.js/discussions/11484
-*/
+const User = ({ name, email }) => (
+  <Layout>
+    <Typography color="textPrimary" variant="h3" gutterBottom>
+      User Info
+    </Typography>
+    <Card>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {name}
+        </Typography>
+        <Typography color="textSecondary">{email}</Typography>
+      </CardContent>
+    </Card>
+  </Layout>
+);
 
-export default function User() {
-  const router = useRouter();
-  const { id } = router.query;
-  const { name, email } =
-    useQuery(GET_USER, {
-      variables: { id },
-    }).data?.user || {};
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+};
 
-  return (
-    <Layout>
-      <Typography color="textPrimary" variant="h3" gutterBottom>
-        User Info
-      </Typography>
-      <Card>
-        <CardContent>
-          <Typography variant="h5" component="h2">
-            {name}
-          </Typography>
-          <Typography color="textSecondary">{email}</Typography>
-        </CardContent>
-      </Card>
-    </Layout>
-  );
+export default User;
+
+export async function getServerSideProps(ctx) {
+  const { id } = ctx.params;
+  const {
+    data: {
+      user: { name, email },
+    },
+  } = await client.query({
+    query: GET_USER,
+    variables: { id },
+  });
+  return {
+    props: { name, email },
+  };
 }
