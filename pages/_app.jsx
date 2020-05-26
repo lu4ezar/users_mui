3a9b7e52bf7deb654895bbf4953fa7500feccb59
lu4ezar <misha-shisha@mail.ru>
2020-05-26 20:07:16 +0700

    + LinearProgress for route changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,13 +2,30 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import PropTypes from "prop-types";
 import "../styles/global.css";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, LinearProgress } from "@material-ui/core";
 import { ApolloProvider } from "@apollo/react-hooks";
+import Router from "next/router";
+import { useState } from "react";
 import client from "../src/apolloClient";
 
 export default function App({ Component, pageProps }) {
+  const [isLoadingPage, setIsLoadingPage] = useState(false);
+  Router.events.on("routeChangeStart", () => {
+    setIsLoadingPage(true);
+  });
+  Router.events.on("routeChangeComplete", () => {
+    setIsLoadingPage(false);
+  });
+  Router.events.on("routeChangeError", () => {
+    setIsLoadingPage(false);
+  });
   return (
     <ApolloProvider client={client}>
+      {isLoadingPage ? (
+        <LinearProgress />
+      ) : (
+        <LinearProgress style={{ visibility: "hidden" }} />
+      )}
       <CssBaseline />
       <Component {...pageProps} />
     </ApolloProvider>
