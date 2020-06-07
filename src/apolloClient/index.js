60d249601edb8a2c92877917486209fbf532a2c7
lu4ezar <misha-shisha@mail.ru>
2020-06-07 13:56:37 +0700

    switch to apollo-boost

--- src/apolloClient/index.js
+++ src/apolloClient/index.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-console */
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import { onError } from "apollo-link-error";
-import { ApolloLink } from "apollo-link";
+import ApolloClient from "apollo-boost";
 
 export const ENDPOINT =
   process.env.NODE_ENV === "development"
@@ -11,22 +7,7 @@ export const ENDPOINT =
     : `https://${process.env.VERCEL_URL}/api/graphql`;
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors)
-        graphQLErrors.forEach(({ message, locations, path }) =>
-          console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-          )
-        );
-      if (networkError) console.log(`[Network error]: ${networkError}`);
-    }),
-    new HttpLink({
-      uri: ENDPOINT,
-      credentials: "same-origin",
-    }),
-  ]),
-  cache: new InMemoryCache(),
+  uri: ENDPOINT,
 });
 
 export default client;
